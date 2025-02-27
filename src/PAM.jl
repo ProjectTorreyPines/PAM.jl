4d0ec61a699877520c04c10fb41c0a44c7648f7d
Orso Meneghini <orso82@gmail.com>
2025-02-27 11:12:22 -0800

    bug fix

--- src/PAM.jl
+++ src/PAM.jl
@@ -645,6 +645,8 @@ function run_PAM(dd::IMAS.dd; t_start::Float64, t_finish::Float64, time_step::Fl
 end
 
 export run_PAM
+
+const document = Dict()
 document[Symbol(@__MODULE__)] = [name for name in Base.names(@__MODULE__; all=false, imported=false) if name != Symbol(@__MODULE__)]
 
 end
